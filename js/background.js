50910a9b5073906469f7c102a395bf910f1f6f5d
Sw4rZ <lesoaron07@gmail.com>
2025-04-12 21:33:16 +0530

    Update and rename exploitx.html to js/background.js

---
+++ js/background.js
@@ -0,0 +1,7 @@
+// Background effect (already styled in CSS)
+document.addEventListener("DOMContentLoaded", () => {
+  const bg = document.querySelector(".background-animation");
+  if (bg) {
+    // Extra effects can be added here later
+  }
+});
