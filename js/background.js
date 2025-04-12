ba770a23cab9f1ee32cc20a0fb8244c43d3dbc74
Sw4rZ <lesoaron07@gmail.com>
2025-04-12 21:56:37 +0530

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,7 +1,6 @@
-// Background effect (already styled in CSS)
 document.addEventListener("DOMContentLoaded", () => {
   const bg = document.querySelector(".background-animation");
   if (bg) {
-    // Extra effects can be added here later
+    // Optional enhancement later
   }
 });
