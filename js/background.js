3f3f6bdf598ba9da6acbcfc5b8ac32b53a06050b
Sw4rZ <lesoaron07@gmail.com>
2025-04-13 16:20:24 +0530

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,6 +1,44 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const bg = document.querySelector(".background-animation");
-  if (bg) {
-    // Optional enhancement later
-  }
-});
+const canvas = document.getElementById("bgCanvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const particles = [];
+
+function Particle() {
+  this.x = Math.random() * canvas.width;
+  this.y = Math.random() * canvas.height;
+  this.radius = Math.random() * 2;
+  this.speedX = (Math.random() - 0.5) * 0.5;
+  this.speedY = (Math.random() - 0.5) * 0.5;
+}
+
+Particle.prototype.update = function () {
+  this.x += this.speedX;
+  this.y += this.speedY;
+
+  if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
+  if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+};
+
+Particle.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+  ctx.fillStyle = "#00ffcc";
+  ctx.fill();
+};
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  particles.forEach((p) => {
+    p.update();
+    p.draw();
+  });
+  requestAnimationFrame(animate);
+}
+
+for (let i = 0; i < 100; i++) {
+  particles.push(new Particle());
+}
+
+animate();
